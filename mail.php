887aea0158c2cbf414adf84155ac90dd7be0f6be
buwaneka sirinath <bsirinath@gmail.com>
2021-05-28 15:21:07 +0530

    Font fix

--- mail.php
+++ mail.php
@@ -48,7 +48,7 @@ function smtpmailer($to, $from, $from_name, $subject, $body)
 
 <html>
     <head>
-        <title>PHPMailer 5.2 testing from Markstrat</title>
+        <title>Markstrat</title>
     </head>
     <body style="background: black;">
         <center><h2 style="padding-top:70px;color: white;"><?php echo $error; ?></h2></center>
